42662d15162d3a58af4bdab5ba5d69e169f73388
Jedediah Micka <jmicka@augustus.home>
2016-04-04 23:05:17 +0100

    Initial commit
    - includes shared common module for iOS and OSX codebase

---
+++ ios.common/ios_common.h
@@ -0,0 +1,17 @@
+//
+//  ios.common.h
+//  ios.common
+//
+//  Created by Jedediah Micka on 4/4/16.
+//  Copyright © 2016 Jedediah Micka. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for ios.common.
+FOUNDATION_EXPORT double ios_commonVersionNumber;
+
+//! Project version string for ios.common.
+FOUNDATION_EXPORT const unsigned char ios_commonVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ios_common/PublicHeader.h>
---
+++ ios/ViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ViewController.swift
+//  ios
+//
+//  Created by Jedediah Micka on 4/1/16.
+//  Copyright © 2016 Jedediah Micka. All rights reserved.
+//
+
+import UIKit;
+import ios_common;
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        
+        let foo = CommonClass();
+        
+        print(foo.getString());
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
