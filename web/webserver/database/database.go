b3d6e004ed00652eacd05fd0d94e3cabf96f36b8
jmicka <jmicka@jedmicka.com>
2016-09-27 00:57:39 +0100

    Incorporating code-review changes
    - restructured order of exposed methods in database.go
    - cleaned up routing table init

--- web/webserver/database/database.go
+++ web/webserver/database/database.go
@@ -9,6 +9,17 @@ import (
 
 var pool *pgx.ConnPool
 
+// Establish pool of DB connections
+func Establish_Connection_Pool() {
+
+	var error error
+	pool, error = pgx.NewConnPool(read_db_configuration())
+	if error != nil {
+		fmt.Fprintf(os.Stderr, "Unable to establish connection pool to database: %v\n", error)
+		os.Exit(1)
+	}
+}
+
 func Execute_Query(statement string) (*pgx.Rows, error) {
 	conn, error := pool.Acquire()
 	if error != nil {
@@ -20,17 +31,6 @@ func Execute_Query(statement string) (*pgx.Rows, error) {
 	rows, _ := conn.Query(statement)
 
 	return rows, nil
-}
-
-// Connect to the database
-func Establish_Connection_Pool() {
-
-	var error error
-	pool, error = pgx.NewConnPool(read_db_configuration())
-	if error != nil {
-		fmt.Fprintf(os.Stderr, "Unable to establish connection pool to database: %v\n", error)
-		os.Exit(1)
-	}
 }
 
 func read_db_configuration() pgx.ConnPoolConfig {
--- web/webserver/routing_table/routing_table.go
+++ web/webserver/routing_table/routing_table.go
@@ -12,17 +12,12 @@ import (
 func init() {
 	// This does not work for routes where the path matches, but the method does not (on HEAD and OPTIONS need to check)
 	// https://github.com/julienschmidt/httprouter/issues/13
-//	var 405_response http.HandlerFunc = error_handler.Generate(405)
 	router.Instance().HandleMethodNotAllowed = true
 	router.Instance().MethodNotAllowed = error_handler.Generate(405)
-
-//	var 404_response http.HandlerFunc = error_handler.Generate(404)
 	router.Instance().NotFound = error_handler.Generate(404)
 
 	// Required so the trailing slash is not redirected
 	router.Instance().RedirectTrailingSlash = false
-
-	// Serve static files, no directory browsing
 }
 
 func Load() {
